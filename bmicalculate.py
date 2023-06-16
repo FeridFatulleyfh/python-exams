e7d815f56446364ac4fd84577ce04b2cf8cc1453
FeridFatulleyfh <136753214+FeridFatulleyfh@users.noreply.github.com>
2023-06-16 21:46:48 +0400

    Create bmicalculate.py

---
+++ bmicalculate.py
@@ -0,0 +1,6 @@
+kilo = float(input("Cekinizi Daxil Edin : "))
+hundurluk = float(input("Boyunuzu Daxil Edin: "))
+
+BMI = kilo / (hundurluk/100)**2
+
+print(f"Sənin çəkin {kilo}kg , Hündürlüyün {hundurluk}sm dır. BMI : {BMI}")
